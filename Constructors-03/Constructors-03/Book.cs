02e447f65a55163b3b347e817bfd33853d9901d4
mahdi <mahdihasanvandi2540@gmail.com>
2025-06-08 17:08:36 +0330

    constrauctor add

---
+++ Constructors-03/Constructors-03/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_03
+{
+    public class Book
+    {
+      private string _title;
+      private string _author;
+      private double _price;
+
+
+        public Book()
+        {
+            _title = "defult";
+            _author = "defult";
+            _price = 0;
+        }
+
+        public Book(string title,string author,double price)
+        {
+            this._title = title;
+            this._author = author;
+            this._price = price;
+        }
+
+        /// <summary>
+        /// نمایش اطلاعات
+        /// </summary>
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Title:{_title}");
+            Console.WriteLine($"Title:{_author}");
+            Console.WriteLine($"Title:{_price}");
+
+        }
+    }
+}
---
+++ Constructors-03/Constructors-03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Book book1 = new Book("MasnaviManavi","Molana",999.9);
+
+
+
+            book.PrintInfo();
+            book1.PrintInfo();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
