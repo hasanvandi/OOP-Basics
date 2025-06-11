99675346519307aa5ba7e7ea3209a5c434f129af
mahdi <mahdihasanvandi2540@gmail.com>
2025-06-11 11:21:28 +0330

    Rename files and foders

---
+++ Example_shart-04/Example_shart-04/Product.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Example_shart_04
+{
+    public class Product
+    {
+        private string _name;
+        private double _price;
+        private int _stock;
+
+
+
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public double Price
+        {
+            get
+            {
+              return _price;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Please enter a right amount");
+                    _price = 0;
+
+                }
+                else
+                {
+                    _price = value;
+
+                }
+            }
+
+        }
+
+        public int Stock 
+        {
+            get 
+            { 
+                return _stock;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Please enter a right amount");
+                    _stock = 0;
+                }
+                else
+                {
+                    _stock = value;
+                }
+            }
+        }
+
+
+        public Product()
+        {
+           Name = "Name";
+           Price = 0;
+           Stock = 0;
+        }
+
+
+        public Product(string name, double price, int stock)
+        {
+           Name = name;
+           Price = price;
+           Stock = stock;
+        }
+
+    }
+}
---
+++ Example_shart-04/Example_shart-04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_shart_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            Product product1 = new Product("book",158.3,3);
+
+            Product product2 = new Product("Mose", -200, -3);
+
+
+            Console.WriteLine($"Name:{product.Name}\nPrice:{product.Price}\nStock:{product.Stock}");
+
+            Console.WriteLine($"Name:{product1.Name}\nPrice:{product1.Price}\nStock:{product1.Stock}");
+
+            Console.WriteLine($"Name:{product2.Name}\nPrice:{product2.Price}\nStock:{product2.Stock}");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
